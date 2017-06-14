db193e418d0430071297f2dccd058d45e2bf412a
yuanquan569 <yyliao91@qq.com>
2017-06-14 23:43:01 +0800

    First PUSH

---
+++ AndroidUtils/app/src/main/java/com/yuan/androidutils/utils/LogUtils.java
@@ -0,0 +1,26 @@
+package com.yuan.androidutils.utils;
+
+/**
+ * Created by yuan on 2017/5/8.
+ */
+
+public class LogUtils {
+
+    public static void v(String TAG, String text){
+        
+    }
+
+    public static void i(String TAG, String text){
+
+    }
+    public static void d(String TAG, String text){
+
+    }
+    public static void w(String TAG, String text){
+
+    }
+    public static void e(String TAG, String text){
+
+    }
+
+}
